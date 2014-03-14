90f138103afdef705b8683a90c9313b389ce2ecc
Michael Bayne <mdb@samskivert.com>
2014-03-14 10:45:58 -0700

    Out with f*ing JavaFX tabs.
    The focus issues were super annoying and it's not really that useful to see
    what buffers you have open across the top of the view. If you're even remotely
    hardcore there will be too many anyway.
    
    Eventually I'd like to deemphasize the notion of "open" buffers anyway, and
    move to a "go to this file in this project" intent, which won't care if the
    file is open yet or not.

--- src/main/scala/scaled/impl/EditorPane.scala
+++ src/main/scala/scaled/impl/EditorPane.scala
@@ -8,15 +8,12 @@ import java.io.File
 
 import scala.collection.mutable.ArrayBuffer
 
-import javafx.application.{Application, Platform}
-import javafx.event.{Event, EventHandler}
-import javafx.scene.Node
-import javafx.scene.control.{Label, Tab, TabPane}
-import javafx.scene.layout.{BorderPane, HBox, Priority, VBox}
-import javafx.scene.paint.Color
+import javafx.application.Application
+import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 
-import reactual.{Future, Value}
+import reactual.Value
 
 import scaled._
 import scaled.major.TextMode
@@ -31,15 +28,12 @@ import scaled.major.TextMode
   */
 class EditorPane (app :Application, stage :Stage) extends BorderPane with Editor {
 
-  private case class OpenBuffer (tab :Tab, view :BufferViewImpl) {
+  private case class OpenBuffer (content :BorderPane, area :BufferArea, view :BufferViewImpl) {
     def buffer = view.buffer
     def name = view.buffer.name
     override def toString = name
   }
   private val _buffers = ArrayBuffer[OpenBuffer]()
-
-  private val _tabs = new TabPane()
-  setCenter(_tabs)
 
   private val _mini :Minibuffer.Area = {
     val (miniPrompt :Label, mini :Minibuffer.Area) = Minibuffer.create(this)
@@ -81,8 +75,7 @@ class EditorPane (app :Application, stage :Stage) extends BorderPane with Editor
     case Some(ob) => _focus.update(ob)
     // if we find no buffer, create a new one with the specified name
     case None =>
-      def dirFor (file :File) = if (file.isDirectory) file else file.getParentFile
-      val file = _buffers.headOption map(_.buffer.file) map(dirFor) getOrElse(cwd())
+      val file = _buffers.headOption map(_.buffer.dir) getOrElse(cwd())
       newBuffer(BufferImpl.empty(buffer, file))
   }
 
@@ -100,48 +93,35 @@ class EditorPane (app :Application, stage :Stage) extends BorderPane with Editor
       override def createMode () = new TextMode(EditorPane.this, view, this)
     }
 
-    val tab = new Tab()
     val content = new BorderPane()
-    content.setCenter(new BufferArea(this, view, disp))
+    val area = new BufferArea(this, view, disp)
+    content.setCenter(area)
     content.setBottom(new ModeLine(this, view))
-    tab.setContent(content)
 
-    val obuf = OpenBuffer(tab, view)
-    tab.setOnCloseRequest(new EventHandler[Event]() {
-      def handle (ev :Event) = { killBuffer(obuf) ; ev.consume() }
-    })
+    val obuf = OpenBuffer(content, area, view)
     _buffers prepend obuf
-    // TODO: if this tab is closed via the UI, remove our OB from _buffers
-    view.buffer.nameV onValueNotify tab.setText
-
-    // TODO: this doesn't work for mouse based selection; need to implement hacky workaround, or
-    // maybe we'll roll our own TabPane since we don't want a lot of the other fiddly business
-    tab.selectedProperty.addListener(onChangeB { isSel =>
-      if (isSel) _focus.update(obuf)
-    })
-    _tabs.getTabs.add(tab)
     _focus.update(obuf)
   }
 
   private def killBuffer (obuf :OpenBuffer) {
     // TODO: run buffer kill hooks to determine if it should be killed
     _buffers -= obuf
-    _tabs.getTabs.remove(obuf.tab)
 
     // if our last buffer is killed, create a new scratch buffer
-    if (_tabs.getTabs.isEmpty) newBuffer(BufferImpl.scratch("*scratch*"))
+    if (_buffers.isEmpty) newBuffer(BufferImpl.scratch("*scratch*"))
+    // otherwise if the killed buffer was focused, display the most recently edited buffer
+    else if (_focus.get == obuf) _focus.update(_buffers.head)
   }
 
   private def onFocusChange (buf :OpenBuffer) {
-    if (buf == null) _mini.requestFocus()
+    if (buf == null) {
+      _mini.requestFocus()
+      _mini.toFront()
+    }
     else {
-      // make sure the focused buffer's tab is visible and has JavaFX focus (note: we can't request
-      // focus immediately, we have to defer it to the next FX UI tick)
-      if (_tabs.getSelectionModel.getSelectedItem == buf.tab) defer {
-        // focus may have changed while we were being deferred, so double check
-        if (_focus.get == buf) buf.tab.getContent.requestFocus()
-      }
-      else _tabs.getSelectionModel.select(buf.tab)
+      setCenter(buf.content)
+      buf.content.toFront()
+      buf.area.requestFocus()
       // also move the focused buffer to the head of the buffers
       _buffers -= buf
       _buffers prepend buf
