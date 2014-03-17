718165ef2905ff3f1d8c8dbd5666de89f79c3b72
Michael Bayne <mdb@samskivert.com>
2014-03-17 16:04:14 -0700

    Some special handling for non-existent files.
    We still want as "canonical" as possible a path even for files that don't
    exist. So if you edit "foo/bar" we want to prepend the cwd before passing it
    on. As the comment says, it would be nice if the caller didn't have to do this,
    but once we turn it into a file this becomes fiddly.
    
    This is a stopgap solution and I'll revisit at some point. I may yet ditch File
    entirely and use NIO Path...

--- src/main/scala/scaled/impl/Main.scala
+++ src/main/scala/scaled/impl/Main.scala
@@ -13,7 +13,12 @@ class Main extends Application {
   override def start (stage :Stage) {
     val epane = new EditorPane(this, stage)
     // open a pane/tab for each file passed on the command line
-    getParameters.getRaw foreach { p => epane.newBuffer(new File(p)) }
+    getParameters.getRaw foreach { p =>
+      // TODO: should Editor just take a path and do this massaging internally? or should we export
+      // a public Files utility class and recommend callers use that? meh...
+      val f = new File(p)
+      epane.newBuffer(if (f.exists || f.isAbsolute) f else new File(cwd(), p))
+    }
 
     val scene = new Scene(epane)
     // TODO: how to support themes, etc.?
