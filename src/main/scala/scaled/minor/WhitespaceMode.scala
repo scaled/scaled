af23ddd53ed34f50b5a2614f4885807b8b37a8ac
Michael Bayne <mdb@samskivert.com>
2014-03-19 09:55:20 -0700

    Beginnings of a whitespace mode.
    It will highlight undesirable whitespace, provide fns for deleting said
    whitespace, &c.

---
+++ src/main/scala/scaled/minor/WhitespaceMode.scala
@@ -0,0 +1,72 @@
+//
+// Scaled - a scalable editor extensible via JVM languages
+// http://github.com/samskivert/scaled/blob/master/LICENSE
+
+package scaled.minor
+
+import scala.annotation.tailrec
+
+import scaled._
+import scaled.major.{EditingMode, Syntax}
+
+object WhitespaceConfig extends ConfigDefs {
+
+  val showTrailingWhitespace = key(
+    "If true, trailing whitespace will be highlighted", true)
+
+  val trailingWhitespaceFace = key(
+    "The face in which to display trailing whitespace.", Face().withBG("#FF0000"))
+}
+
+/** A minor mode that provides whitespace manipulation fns and can highlight undesirable
+  * whitespace.
+  */
+class WhitespaceMode (editor :Editor, config :Config, view :RBufferView, major :EditingMode)
+    extends MinorMode {
+  import WhitespaceConfig._
+
+  // respond to buffer and line edits
+  view.buffer.edited onValue onBufferEdit
+  view.buffer.lineEdited onValue onLineEdit
+
+  // respond to toggling of the trailing whitespace config
+  config.value(showTrailingWhitespace) onValueNotify updateBufferTrailingWhitespace
+
+  override def name = "whitespace"
+  override def keymap = Seq() // TODO
+  override def dispose () {
+    // TODO: updateBufferTrailingWhitespace(false)?
+  }
+
+  protected def onBufferEdit (edit :Buffer.Edit) {
+    if (config(showTrailingWhitespace)) {
+      edit.offset until (edit.offset+edit.added) foreach updateLineTrailingWhitespace(true)
+    }
+  }
+
+  protected def onLineEdit (edit :Line.Edit) {
+    if (config(showTrailingWhitespace) && edit.added > 0) {
+      updateLineTrailingWhitespace(true)(edit.loc.row)
+    }
+  }
+
+  protected def updateBufferTrailingWhitespace (show :Boolean) {
+    0 until view.buffer.lines.size foreach updateLineTrailingWhitespace(show)
+  }
+  protected def updateLineTrailingWhitespace (show :Boolean)(ii :Int) {
+    val line = view.buffer.lines(ii)
+    @tailrec def seek (col :Int) :Int = {
+      if (col == 0 || major.syntax(line.charAt(col-1)) != Syntax.Whitespace) col
+      else seek(col-1)
+    }
+    val last = line.length
+    val first = seek(last)
+    if (first < last) {
+      // TODO: setting the face to default is presumptuous, really we want to remove the trailing
+      // whitespace face if it's applied and leave it alone otherwise; I'm not sure I want to
+      // support multiple faces per character, but I may have to go down that road...
+      val face = if (show) config(trailingWhitespaceFace) else Face.defaultFace
+      view.buffer.applyFace(face, Loc(ii, first), Loc(ii, last))
+    }
+  }
+}
