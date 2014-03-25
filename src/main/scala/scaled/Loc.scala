45ed062568d899949d4c1d828989825684e86255
Michael Bayne <mdb@samskivert.com>
2014-03-25 14:47:57 -0700

    No need for Loc.at() now that it's a value class.

--- src/main/scala/scaled/Loc.scala
+++ src/main/scala/scaled/Loc.scala
@@ -34,27 +34,32 @@ class Loc private (val rowCol :Long) extends AnyVal {
   def greater (other :Loc) :Loc = if (this > other) this else other
 
   /** Returns a loc adjusted by `deltaRow` rows and `deltaCol` columns. */
-  def + (deltaRow :Int, deltaCol :Int) :Loc = at(row+deltaRow, col+deltaCol)
+  def + (deltaRow :Int, deltaCol :Int) :Loc = Loc(row+deltaRow, col+deltaCol)
+
+  /** "Adds" `region` to `this` loc. This returns the loc immediately after `region`, assuming
+    * `region` exists in a buffer at `this` loc. */
+  def + (region :Seq[LineV]) :Loc = region.size match {
+    case 0 => this
+    case 1 => Loc(row, col+region.head.length)
+    case n => Loc(row + n-1, region.last.length)
+  }
+
   /** Returns a loc on row `row` at this loc's column. */
-  def atRow (row :Int) :Loc = at(row, col)
+  def atRow (row :Int) :Loc = Loc(row, col)
   /** Returns a loc on column `col` at this loc's row. */
-  def atCol (col :Int) :Loc = at(row, col)
-  /** Returns this `loc` if `row` and `col` are equal to its values, otherwise returns a new `Loc` at
-    * the specified coordinates. Useful for bounding locations without superfluous allocation. */
-  def at (row :Int, col :Int) :Loc =
-    if (this.row == row && this.col == col) this else Loc(row, col)
+  def atCol (col :Int) :Loc = Loc(row, col)
 
   /** Returns the loc directly to the left of this loc. */
-  def prevC :Loc = at(row, col-1)
+  def prevC :Loc = Loc(row, col-1)
   /** Returns the loc directly to the right of this loc. */
-  def nextC :Loc = at(row, col+1)
+  def nextC :Loc = Loc(row, col+1)
   /** Returns the loc directly above this loc. */
-  def prevL :Loc = at(row-1, col)
+  def prevL :Loc = Loc(row-1, col)
   /** Returns the loc directly below this loc. */
-  def nextL :Loc = at(row+1, col)
+  def nextL :Loc = Loc(row+1, col)
 
   /** Returns the start of the line after the line referenced by this loc. */
-  def nextStart :Loc = at(row+1, 0)
+  def nextStart :Loc = Loc(row+1, 0)
 
   override def toString = s"r$row:c$col"
 }
