e5631c33140e123572470e03aedf1a802bcc6df0
Michael Bayne <mdb@samskivert.com>
2014-04-01 09:32:46 -0700

    Wired up scanning for annotated classes.
    Right now we only scan the "built-in" package, but it will be trivial to start
    scanning external packages once I sort out how the directory structure will
    work there.
    
    I kind of want just a single top-level directory for each package:
    
    .../Packages/whitespace-mode
    .../Packages/project-service
    .../Packages/scaled-scala-mode
    
    but the packages are actually identified by a version control URL, so it might
    be better to have:
    
    .../Packages/github.com/scaled/whitespace-mode
    .../Packages/github.com/scaled/project-service
    .../Packages/code.google.com/p/scaled-scala-mode
    
    but if I do the latter, then I need some sort of marker file at the top-level
    of each package. Now that I say this out loud, I realize that the latter
    approach is clearly better and the marker file will be useful for other
    reasons. Make it so!

--- src/main/scala/scaled/impl/PackageManager.scala
+++ src/main/scala/scaled/impl/PackageManager.scala
@@ -6,59 +6,131 @@ package scaled.impl
 
 import java.io.File
 import java.net.URLClassLoader
+import java.nio.file.Files
 
-import scala.collection.mutable.{ArrayBuffer, Set => MSet}
+import scala.collection.mutable.{ArrayBuffer, Set => MSet, Map => MMap}
 
-import org.objectweb.asm.ClassReader
+import org.objectweb.asm.{AnnotationVisitor, ClassReader, ClassVisitor, Opcodes}
 
 class PackageManager (metaDir :File) {
 
-  val pkgsDir = Filer.requireDir(new File(metaDir, "Packages"))
+  /** Resolves the class for the mode named `name`. */
+  def mode (name :String) :Option[Class[_]] = modes.get(name).map(_.mode(name))
+
+  /** Resolves the class for the service with classname `name`. */
+  def service (name :String) :Option[Class[_]] = services.get(name).map(_.service(name))
+
+  private val pkgsDir = Filer.requireDir(new File(metaDir, "Packages"))
+  private val pkgs = MMap[String,Package]()
+
+  private val modes = MMap[String,Package]()
+  private val services = MMap[String,Package]()
 
   // resolve our "built-in" package, which we locate via the classloader
   getClass.getClassLoader.asInstanceOf[URLClassLoader].getURLs foreach { url =>
     if ((url.getProtocol == "file") && !(url.getPath endsWith ".jar")) {
-      resolvePackage("scaled", new File(url.getPath))
+      addPackage(new Package("scaled", new File(url.getPath)))
     }
   }
 
-  // TODO: extract these mode mappings via metadata found in the classpath
-  val modes = Map(
-    "text"         -> "scaled.major.TextMode",
-    "mini-read"    -> "scaled.major.MiniReadMode",
-    "mini-yesno"   -> "scaled.major.MiniYesNoMode",
-    "mini-readopt" -> "scaled.major.MiniReadOptMode",
-    "mini-isearch" -> "scaled.major.ISearchMode"
-  )
+  // TODO: resolve all packages in the pkgsDir
 
-  def mode (name :String) :Option[Class[_]] = {
-    modes.get(name).map(Class.forName)
+  private def addPackage (pkg :Package) {
+    pkgs += (pkg.name -> pkg)
+    pkg.modes.keys foreach { m => modes += (m -> pkg) }
+    pkg.services foreach { s => services += (s -> pkg) }
+    // println(s"Added package $pkg")
   }
 
-  def service (name :String) :Option[Class[_]] = {
-    None
-  }
+  private class Package (val name :String, root :File) {
 
-  // private class Package (name :String, root :File) {
-  //   val (majorModes, minorModes, services) = {
-  //     // scan the package directory locating FooMode.class and FooService.class
-  //     val (mfiles, sfiles) = (ArrayBuffer[File](), ArrayBuffer[File]())
-  //     Filer.descendFiles(root) { f =>
-  //       if (f.getName.endsWith("Mode.class")) mfiles += f
-  //       if (f.getName.endsWith("Service.class")) sfiles += f
-  //     }
+    /** Loads and returns the class for the mode named `name`. */
+    def mode (name :String) :Class[_] = loader.loadClass(modes(name))
+    /** Loads and returns the class for the service with class name `name`. */
+    def service (name :String) :Class[_] = loader.loadClass(name)
 
-  //     val depends = MSet[String]()
+    val modes = MMap[String,String]() // mode -> classname for all this package's modes
+    val services = ArrayBuffer[String]() // service classname for all this package's services
+    val depends = MSet[String]() // name of all packages on which we depend
+    val classes = root // the directory that contains our classes
 
-  //     val (majors, minors, services) = (ArrayBuffer[
-  //     // now parse the bytecode for these classes to extract and process those which are annotated
-  //     // with @Mode and @Service
-  //     mfiles foreach { mf =>
-  //     }
-  //   }
-  // }
+    /** The class loader for classes in this package. */
+    val loader :ClassLoader = new URLClassLoader(Array(classes.toURI.toURL)) {
+      override protected def findClass (name :String) :Class[_] = {
+        var pkgs = dependPkgs // first try finding the class in our dependencies
+        while (!pkgs.isEmpty) {
+          try return pkgs.head.loader.loadClass(name)
+          catch {
+            case cnfe :ClassNotFoundException => pkgs = pkgs.tail
+          }
+        }
+        super.findClass(name) // then fall back to looking locally
+      }
+    }
+    // TODO: make sure dependent packages are resolved sooner?
+    private lazy val dependPkgs = depends.map(pkgs).toList
 
-  private def resolvePackage (name :String, root :File) {
-    println(s"TODO, resolve ${root.getAbsolutePath}")
+    override def toString = s"$name [modes=${modes.keys}, svcs=$services, deps=$depends]"
+
+    // scan the package directory locating FooMode.class and FooService.class
+    Filer.descendFiles(root) { f =>
+      try {
+        if (f.getName.endsWith("Mode.class")) parseMode(f)
+        if (f.getName.endsWith("Service.class")) parseService(f)
+      } catch {
+        case e :Exception => println("Error parsing $f: $e")
+      }
+    }
+
+    private abstract class Visitor (aclass :String) extends ClassVisitor(Opcodes.ASM5) {
+      private var _cname :String = _
+      override def visit (version :Int, access :Int, name :String, signatur :String,
+                          superName :String, ifcs :Array[String]) {
+        _cname = name.replace('/', '.') // TODO: handle inner classes?
+      }
+
+      override def visitAnnotation (desc :String, viz :Boolean) = {
+        if (desc != aclass) null
+        else {
+          onMatch(_cname)
+          new AnnotationVisitor(Opcodes.ASM5) {
+            override def visit (name :String, value :AnyRef) {
+              onAnnotation(_cname, name, value.toString)
+            }
+            override def visitArray (name :String) = {
+              if (name != "deps") null
+              else new AnnotationVisitor(Opcodes.ASM5) {
+                override def visit (name :String, value :AnyRef) {
+                  depends += value.toString
+                }
+              }
+            }
+          }
+        }
+      }
+
+      def onMatch (cname :String) {}
+      def onAnnotation (cname :String, aname :String, value :String) :Unit
+    }
+    private def parse (file :File, viz :Visitor) {
+      val r = new ClassReader(Files.readAllBytes(file.toPath))
+      r.accept(viz, ClassReader.SKIP_CODE|ClassReader.SKIP_DEBUG|ClassReader.SKIP_FRAMES)
+    }
+
+    private def parseMode (file :File) = parse(file, new Visitor("Lscaled/Mode;") {
+      override def onAnnotation (cname :String, aname :String, value :String) {
+        if (aname == "name") modes += (value.toString -> cname)
+        // TODO: put the description somewhere?
+      }
+    })
+
+    private def parseService (file :File) = parse(file, new Visitor("Lscaled/Service;") {
+      override def onMatch (cname :String) {
+        services += cname
+      }
+      override def onAnnotation (cname :String, aname :String, value :String) {
+        // TODO: put the description somewhere?
+      }
+    })
   }
 }
