52fde3f8ccb7c3f639b4659ba1c27ad308eaaaa3
Michael Bayne <mdb@samskivert.com>
2014-04-29 15:42:35 -0700

    Added text styles.
    Text modes can extend TextMode and make use of said styles. We'll probably
    expand this as I get a better handle on what sorts of things are reasonably
    "standard" for text modes.

--- src/main/scala/scaled/major/TextMode.scala
+++ src/main/scala/scaled/major/TextMode.scala
@@ -6,10 +6,25 @@ package scaled.major
 
 import scaled._
 
+object TextConfig extends Config.Defs {
+
+  /** The CSS style applied to `header` lines. */
+  val headerStyle = "textHeaderFace"
+  /** The CSS style applied to `subHeader` lines. */
+  val subHeaderStyle = "textSubHeaderFace"
+  /** The CSS style applied to `section` lines. */
+  val sectionStyle = "textSectionFace"
+  /** The CSS style applied to `list` lines. */
+  val listStyle = "textListFace"
+}
+
 @Major(name="text", tags=Array("text"), desc="""
   A major mode for editing plain text. This is used when no more specific mode can be found.
 """)
 class TextMode (env :Env) extends EditingMode(env) {
+
+  override def configDefs = TextConfig :: super.configDefs
+  override def stylesheets = stylesheetURL("/text.css") :: super.stylesheets
 
   override def dispose () {}  // nothing to dispose
 }
