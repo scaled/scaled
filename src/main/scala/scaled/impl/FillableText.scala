8c6842f134f737b49180c4f6a0d77d7839290a4f
Michael Bayne <mdb@samskivert.com>
2014-04-30 11:37:46 -0700

    Add support for -fx-bg-stroke.

--- src/main/scala/scaled/impl/FillableText.scala
+++ src/main/scala/scaled/impl/FillableText.scala
@@ -15,6 +15,14 @@ import javafx.scene.text.Text
 
 class FillableText (text :String) extends Text(text) {
 
+  /** The stroke to use when rendering this text's background */
+  val bgStroke = new StyleableObjectProperty[Paint](null) {
+    override def invalidated () {} // nada
+    override def getBean = FillableText.this
+    override def getName = "bgStroke"
+    override def getCssMetaData = FillableText.BgStroke
+  }
+
   /** The fill to use when rendering this text's background */
   val bgFill = new StyleableObjectProperty[Paint](null) {
     override def invalidated () {} // nada
@@ -27,12 +35,15 @@ class FillableText (text :String) extends Text(text) {
   fillRect.setManaged(false)
   fillRect.setStroke(null)
   fillRect.layoutXProperty.bind(layoutXProperty)
+  fillRect.strokeProperty().bind(bgStroke)
   fillRect.fillProperty().bind(bgFill)
 
   // fill rect visibility depends on our viz and whether we have a bg fill
   visibleProperty.addListener(onChangeB { viz => updateFillViz() })
+  bgStroke.addListener(onChange[Paint] { nstroke => updateFillViz() })
   bgFill.addListener(onChange[Paint] { nfill => updateFillViz() })
-  private def updateFillViz () = fillRect.setVisible(isVisible && bgFill.getValue != null)
+  private def updateFillViz () = fillRect.setVisible(
+    isVisible && (bgFill.getValue != null || bgStroke.getValue != null))
 
   // we have to do this layout syncing manually (via LineViewImpl), because binding to layoutBounds
   // causes weird JavaFX internal failures and there's no public or protected hook we can tie into
@@ -49,6 +60,12 @@ class FillableText (text :String) extends Text(text) {
 object FillableText {
   import java.util.{ArrayList, Collections, List => JList}
 
+  val BgStroke :CssMetaData[FillableText,Paint] = new CssMetaData[FillableText,Paint](
+    "-fx-bg-stroke", PaintConverter.getInstance(), null) {
+    override def isSettable (n :FillableText) = true
+    override def getStyleableProperty (n :FillableText) = n.bgStroke
+  }
+
   val BgFill :CssMetaData[FillableText,Paint] = new CssMetaData[FillableText,Paint](
     "-fx-bg-fill", PaintConverter.getInstance(), null) {
     override def isSettable (n :FillableText) = true
@@ -57,6 +74,7 @@ object FillableText {
 
   val classCssMetaData :JList[CssMetaData[_ <: Styleable, _]] = {
     val styleables = new ArrayList(Text.getClassCssMetaData())
+    styleables.add(BgStroke)
     styleables.add(BgFill)
     Collections.unmodifiableList(styleables)
   }
