c3bc639771a39cf6092ee11e952a62dfe6b33510
Michael Bayne <mdb@samskivert.com>
2014-05-05 14:23:25 -0700

    Fix Maven resolution; better error reporting.

--- editor/src/main/scala/scaled/impl/pkg/MavenResolver.scala
+++ editor/src/main/scala/scaled/impl/pkg/MavenResolver.scala
@@ -17,11 +17,22 @@ class MavenResolver (log :Logger) {
     */
   def resolveDepend (depend :Depend) :Option[ClassLoader] = {
     val pdep = Dependency(depend.groupId, depend.artifactId, depend.version, depend.kind)
-    for {
-      pfile <- pdep.localPOM
-      pom <- POM.fromFile(pfile)
-      res = new DependResolver(pom)
-    } yield loader(depend, res.resolve(true))
+    pdep.localPOM match {
+      case None => log.log(s"Unable to resolve POM for $pdep") ; None
+      case Some(pfile) => POM.fromFile(pfile) match {
+        case None => log.log(s"Unable to load POM from $pfile") ; None
+        case Some(pom) =>
+          val res = new DependResolver(pom) {
+            // ignore non-compile depends; dependencies loaded here are always at least one step
+            // away from the "root" depend, and hence should follow the standard Maven policy of
+            // excluding all non-compile depends
+            override def rootDepends (forTest :Boolean) =
+              if (forTest) super.rootDepends(forTest)
+              else pom.depends filter(_.scope == "compile")
+          }
+          Some(loader(depend, pdep +: res.resolve(false)))
+      }
+    }
   }
 
   private def loader (orig :Depend, deps :Seq[Dependency]) :URLClassLoader = {
@@ -30,7 +41,6 @@ class MavenResolver (log :Logger) {
       case None => Right(s"Missing local artifact for $dep")
     })
     errs foreach log.log
-    println(s"$orig => $jars")
     new URLClassLoader(jars.map(_.toURI.toURL).toArray)
   }
 
