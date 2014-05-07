689d2e4d8fc7219f687442f5ffa657cd3fc44ad5
Michael Bayne <mdb@samskivert.com>
2014-05-07 07:28:15 -0700

    Expecter fixes and API rejiggering.

--- api/src/main/scala/scaled/util/Expecter.scala
+++ api/src/main/scala/scaled/util/Expecter.scala
@@ -6,6 +6,7 @@ package scaled.util
 
 import java.io.{BufferedReader, File, InputStreamReader, OutputStreamWriter, PrintWriter}
 import java.util.concurrent.Executor
+import scaled.Editor
 
 /** Manages an interaction with a separate process, via its stdin, stdout and stderr.
   * This is something sufficiently common for modes and services to do that Scaled provides this
@@ -19,32 +20,56 @@ import java.util.concurrent.Executor
   */
 abstract class Expecter (exec :Executor, command :String*) {
 
-  /** Sends a line of text to the command's stdin. A newline is automatically appended. */
+  /** Sends a line of text to the subprocess's stdin. A newline is automatically appended. */
   def send (line :String) {
-    assertProcess
     out.println(line)
+    out.flush()
+  }
+
+  /** Initiates an interaction with the subprocess. An interaction consists of sending one or more
+    * lines of text to the subprocess and then piping subprocess output to a supplied responder
+    * until the responder indicates that the interaction is complete.
+    *
+    * @param responder a function that takes (line, isStdErr) and returns true if the interaction
+    * is complete, false otherwise.
+    *
+    * @return true if the interaction was started, false if it was not because another interaction
+    * is still active.
+    */
+  def interact (input :Seq[String], responder :(String, Boolean) => Boolean) :Boolean = {
+    if (_responder != null) false
+    else {
+      _responder = responder
+      input foreach out.println
+      out.flush()
+      true
+    }
+  }
+
+  /** Closes this expecter's output stream. This may trigger termination for your subprocess if it
+    * expects that sort of thing.
+    */
+  def close () {
+    process.getOutputStream.close()
   }
 
   /** Terminates the subprocess forcibly. */
   def kill () {
-    if (process != null) process.destroyForcibly()
+    process.destroyForcibly()
   }
 
   /** Waits for the process to complete and returns its exit code. */
   def waitFor () :Int = {
-    assertProcess
     process.waitFor()
   }
 
-  /** Called when a line of text is received from the process's stdout. */
-  def onOut (line :String) :Unit
-
-  /** Called when a line of text is received from the process's stderr.
-    * Defaults to calling [[onOut]] with the line. */
-  def onErr (line :String) :Unit = onOut(line)
+  /** Called when output is received from the subprocess, but we have no configured responder. */
+  def onUnexpected (line :String, isErr :Boolean) :Unit
 
   /** Called if communication with the process fails. */
   def onFailure (exn :Exception) :Unit
+
+  override def toString = s"Expecter(${command.mkString(" ")})"
 
   /** Returns the cwd to use when invoking the process. Override to customize. */
   protected def cwd :File = new File(System.getProperty("user.dir"))
@@ -53,36 +78,56 @@ abstract class Expecter (exec :Executor, command :String*) {
     * form `name=value`. Defaults to none. Override to customize. */
   protected def env :Array[String] = Array[String]()
 
-  private val process = try {
-    Runtime.getRuntime.exec(command.toArray, env, cwd)
-  } catch {
-    case e :Exception => onFailure(e) ; null
-  }
-  if (process != null) {
-    new Thread() {
-      setDaemon(true)
-      override def run () = read(in, onOut)
-    }.start()
-    new Thread() {
-      setDaemon(true)
-      override def run () = read(err, onErr)
-    }.start()
-  }
+  private val process = Runtime.getRuntime.exec(command.toArray, env, cwd)
+
+  // if the process fails to start, an exception will be thrown before these threads are started;
+  // otherwise they'll run until the process's streams are closed
+  new Thread("Expecter: stdin") {
+    setDaemon(true)
+    override def run () = read(in, false)
+  }.start()
+  new Thread("Expecter: stderr") {
+    setDaemon(true)
+    override def run () = read(err, true)
+  }.start()
 
   private lazy val out = new PrintWriter(new OutputStreamWriter(process.getOutputStream, "UTF-8"))
   private lazy val in = new BufferedReader(new InputStreamReader(process.getInputStream, "UTF-8"))
   private lazy val err = new BufferedReader(new InputStreamReader(process.getErrorStream, "UTF-8"))
 
+  private var _responder :(String, Boolean) => Boolean = _
+
   private def assertProcess :Unit =
     if (process == null) throw new IllegalStateException("Process failed to start")
 
-  private def read (reader :BufferedReader, onLine :String => Unit) :Unit = try {
+  private def read (reader :BufferedReader, isErr :Boolean) :Unit = try {
     while (true) {
       val line = reader.readLine
       if (line == null) return
-      exec.execute(new Runnable() { override def run () = onLine(line) })
+      exec.execute(new Runnable() { override def run () = handleInput(line, isErr) })
     }
   } catch {
     case e :Exception => exec.execute(new Runnable() { override def run () = onFailure(e) })
+  }
+
+  private def handleInput (line :String, isErr :Boolean) {
+    if (_responder == null) onUnexpected(line, isErr)
+    else if (_responder(line, isErr)) _responder = null
+  }
+}
+
+object Expecter {
+
+  /** Returns an expecter that logs output received outside interactions to editor,
+    * prefixed with `ident`.
+    */
+  def inEditor (editor :Editor, prefix :String) :Expecter = new Expecter(editor) {
+    def onUnexpected (line :String, isErr :Boolean) {
+      val kind = if (isErr) "stderr" else "stdout"
+      editor.log(s"$prefix: [$kind] $line")
+    }
+    def onFailure (err :Exception) {
+      editor.log(s"$prefix: expect failure", err)
+    }
   }
 }
--- api/src/test/scala/scaled/util/ExpecterTest.scala
+++ api/src/test/scala/scaled/util/ExpecterTest.scala
@@ -7,22 +7,77 @@ package scaled.util
 import java.util.concurrent.Executor
 import org.junit.Assert._
 import org.junit._
+import scala.collection.mutable.ArrayBuffer
 
 class ExpecterTest {
 
-  val immediate = new Executor() {
-    def execute (r :Runnable) = r.run()
+  class AccumExec extends Executor {
+    private val rs = ArrayBuffer[Runnable]()
+    def execute (r :Runnable) :Unit = rs += r
+    def executeAll () = {
+      rs foreach { _.run() }
+      rs.clear()
+    }
   }
 
-  @Test def textExpecter () {
-    val ex = new Expecter(immediate, "echo", "foo") {
-      override def onOut (line :String) {
+  @Test def testUnexpected () {
+    val exec = new AccumExec()
+    val ex = new Expecter(exec, "echo", "foo") {
+      override def onUnexpected (line :String, isErr :Boolean) {
         assertEquals("foo", line)
       }
-      override def onFailure (err :Exception) {
-        err.printStackTrace(System.err)
+      override def onFailure (exn :Exception) {
+        fail(exn.toString)
       }
     }
+    val rv = ex.waitFor()
+    exec.executeAll()
+    assertEquals(0, rv)
+  }
+
+  @Test def testOneLineInteraction () {
+    val exec = new AccumExec()
+    val ex = new Expecter(exec, "cat") {
+      override def onUnexpected (line :String, isErr :Boolean) {
+        fail(s"Unexpected $line $isErr")
+      }
+      override def onFailure (exn :Exception) {
+        fail(exn.toString)
+      }
+    }
+    ex.interact(Seq("hello"), (line, isErr) => {
+      assertEquals("hello", line)
+      true
+    })
+    ex.close()
     assertEquals(0, ex.waitFor())
+    try exec.executeAll()
+    catch {
+      case t :Throwable => ex.kill() ; throw t
+    }
+    finally {
+    }
+  }
+
+  @Test def testMultilineInteraction () {
+    val exec = new AccumExec()
+    val ex = new Expecter(exec, "tee", "test.txt") {
+      override def onUnexpected (line :String, isErr :Boolean) {
+        fail(s"Unexpected $line $isErr")
+      }
+      override def onFailure (exn :Exception) {
+        fail(exn.toString)
+      }
+    }
+    ex.interact(Seq("howdy", "world"), (line, isErr) => {
+      if ("howdy" == line) false
+      else {
+        if ("world" != line) fail("Unexpected $line $isErr")
+        true
+      }
+    })
+    ex.close()
+    assertEquals(0, ex.waitFor())
+    exec.executeAll()
   }
 }
