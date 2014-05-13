d84076418212264630e5ebf5d17f667fcb74c60f
Michael Bayne <mdb@samskivert.com>
2014-05-13 16:07:53 -0700

    Revamp Commenter to use Syntax; also made better.

--- api/src/main/scala/scaled/code/Commenter.scala
+++ api/src/main/scala/scaled/code/Commenter.scala
@@ -13,40 +13,45 @@ class Commenter (buffer :BufferV) {
   import Chars._
 
   /** The string to prepend to an auto-filled line comment. */
-  def commentPrefix :String = ""
-  /** A matcher on [[commentPrefix]]. */
-  val commentPrefixM = Matcher.exact(commentPrefix)
+  def linePrefix :String = ""
+  /** A matcher on [[linePrefix]]. */
+  val linePrefixM = prefixMatcher(linePrefix)
+
+  /** The string to prepend to an auto-filled block comment. */
+  def blockPrefix :String = ""
+  /** A matcher on [[linePrefix]]. */
+  val blockPrefixM = prefixMatcher(blockPrefix)
 
   /** The string to prepend to an auto-filled doc line. */
   def docPrefix :String = ""
   /** A matcher on [[docPrefix]]. */
-  val docPrefixM = Matcher.exact(docPrefix)
+  val docPrefixM = prefixMatcher(docPrefix)
 
   /** Returns the text to be prepended to a comment about to be auto-wrapped at `at`. */
-  def prefixFor (at :Loc) = {
-    val ss = buffer.stylesNear(at)
-    // TODO: count the number of spaces after the comment prefix on the current line and
-    // duplicate that instead of assuming one space
-    if (ss contains commentStyle) commentPrefix + " "
-    else if (ss contains docStyle) docPrefix + " "
-    else ""
+  def prefixFor (at :Loc) :String = prefixFor(buffer.syntaxNear(at))
+
+  /** Returns the auto-fill comment prefix for the specified syntax. */
+  def prefixFor (syntax :Syntax) :String = {
+    // TODO: have the caller tell us how many spaces they want, then count the number of spaces
+    // after the comment prefix on the filled line and duplicate that instead of assuming one space
+    import Syntax._
+    syntax match {
+      case  LineComment => linePrefix + " "
+      case BlockComment => blockPrefix + " "
+      case   DocComment => docPrefix + " "
+      case            _ => ""
+    }
   }
 
-  /** Returns true if `ss` contains `docStyle` or `commentStyle`. */
-  def isCommentStyle (ss :Styles) :Boolean = (ss contains commentStyle) || (ss contains docStyle)
-
-  /** Returns true if `p` is "inside" a comment. The default implementation checks whether `p` is
-    * styled with `commentFace` or `docFace`, or if `p` is the end if its line, whether the
-    * character preceding `p` is thusly styled.
-    */
-  def inComment (p :Loc) :Boolean = isCommentStyle(buffer.stylesNear(p))
+  /** Returns true if `p` is "inside" a comment. */
+  def inComment (p :Loc) :Boolean = buffer.syntaxNear(p).isComment
 
   /** Returns the column of start of the comment on `line`. If `line` does not contain comments,
     * `line.length` is returned. */
   def commentStart (line :LineV) :Int = {
     val llen = line.length ; var c = 0
     // first skip over non-comments at the start of the line
-    while (c < llen && !isCommentStyle(line.stylesAt(c))) c += 1
+    while (c < llen && !line.syntaxAt(c).isComment) c += 1
     // next skip whitespace
     while (c < llen && isWhitespace(line.charAt(c))) c += 1
     // next skip over the comment or doc prefix and we're done
@@ -58,19 +63,21 @@ class Commenter (buffer :BufferV) {
 
   /** Returns the length of the comment delimiter at `col` of `line`. Returns 0 if no comment
     * delimiter is matched at `col`. This is used to skip over the comment delimiter when scanning
-    * for comment start. The default implementation checks for [[commentPrefix]] or [[docPrefix]].
+    * for comment start. The default implementation checks for [[linePrefix]] or [[docPrefix]].
     */
-  def commentDelimLen (line :LineV, col :Int) :Int =
-    if (line.matches(commentPrefixM, col)) commentPrefixM.matchLength
+  def commentDelimLen (line :LineV, col :Int) :Int = {
+    if (line.matches(linePrefixM, col)) linePrefixM.matchLength
+    else if (line.matches(blockPrefixM, col)) blockPrefixM.matchLength
     else if (line.matches(docPrefixM, col)) docPrefixM.matchLength
     else 0
+  }
 
   /** Generates a comment prefix given the supplied desired comment start column. This combines the
     * appropriate number of spaces with `commentPre` and a single trailing space.
     */
   def commentPre (commentPre :String, startCol :Int) :Line = {
     val spaces = " " * (startCol - commentPre.length - 1)
-    Line(spaces + commentPrefix + " ") // TODO: infer number of trailing spaces?
+    Line(spaces + linePrefix + " ") // TODO: infer number of trailing spaces?
   }
 
   /** Refills the comments region `[start, end)`. `start` may be a line which contains some
@@ -88,8 +95,8 @@ class Commenter (buffer :BufferV) {
       // if we have more than one row in our comment block, just use the second line's prefix as our
       // repeat prefix because it's most likely to be correct
       if (end.row > start.row) buffer.line(start.nextStart).view(0, firstPre.length)
-      // otherwise create a prefix using `commentPrefix` (using "" if we lack one)
-      else commentPre(commentPrefix, firstPre.length)
+      // otherwise create a prefix using the auto-fill prefix for the comment type at firstCol
+      else commentPre(prefixFor(firstLine.syntaxAt(firstCol)), firstPre.length)
     }
 
     // append all of the to-be-filled text to a filler configured with our wrap width
@@ -109,4 +116,7 @@ class Commenter (buffer :BufferV) {
     pres zip filled map { case (pre, line) => pre merge line }
   }
 
+  // returns a non-matching matcher if prefix is unspecified; an exact matcher otherwise
+  private def prefixMatcher (prefix :String) =
+    Matcher.exact(if (prefix == "") "NOTUSED" else prefix)
 }
