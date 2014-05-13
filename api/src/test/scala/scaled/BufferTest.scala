d032c90fd8f56429f0a2f2078fed38e02a4c42ee
Michael Bayne <mdb@samskivert.com>
2014-05-13 09:47:02 -0700

    Some buffer tests.

--- api/src/test/scala/scaled/BufferTest.scala
+++ api/src/test/scala/scaled/BufferTest.scala
@@ -5,6 +5,8 @@
 package scaled
 
 import java.io.File
+import org.junit._
+import org.junit.Assert._
 
 object BufferTest {
 
@@ -16,5 +18,41 @@ object BufferTest {
     def dirty = false
     def lines = _lines
     val maxLineLength = _lines.map(_.length).max
+  }
+}
+
+class BufferTest {
+  import BufferTest._
+
+  val text = Seq("This is some text. Text is very interesting.",
+                 "In this case we want to be sure the find fns work",
+                 "and also don't match erroneously.")
+  val buffer = bufferV("test", text.map(Line.apply))
+
+  @Test def testFindForward () {
+    val mp = Matcher.exact("peanut")
+    assertEquals(Loc.None, buffer.findForward(mp, buffer.start))
+
+    val mm = Matcher.exact("match")
+    val mloc = Loc(2, 15)
+    assertEquals(mloc, buffer.findForward(mm, buffer.start))
+    // make sure a search for "match" exactly on match does match
+    assertEquals(mloc, buffer.findForward(mm, mloc))
+    // make sure a search for "match" just after match doesn't match
+    assertEquals(Loc.None, buffer.findForward(mm, mloc.nextC))
+  }
+
+  @Test def testFindBackward () {
+    val mp = Matcher.exact("peanut")
+    assertEquals(Loc.None, buffer.findBackward(mp, buffer.end))
+
+    val mm = Matcher.exact("match")
+    val mloc = Loc(2, 15)
+    assertEquals(mloc, buffer.findBackward(mm, buffer.end))
+    // make sure a search for "match" exactly on match does not match (findBackward starts
+    // immediately prior to `start`)
+    assertEquals(Loc.None, buffer.findBackward(mm, mloc))
+    // make sure a search for "match" before match also doesn't match
+    assertEquals(Loc.None, buffer.findBackward(mm, mloc.prevC))
   }
 }
