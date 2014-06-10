6c379b8db621ed6aba5ab3a3e38c110b9f505525
Michael Bayne <mdb@samskivert.com>
2014-06-10 14:13:51 -0700

    Provide some basic interaction with pacman.

---
+++ src/main/scala/scaled/PackageService.scala
@@ -0,0 +1,23 @@
+//
+// Scaled API - the API against which Scaled extensions are written
+// http://github.com/scaled/scaled-api/blob/master/LICENSE
+
+package scaled
+
+import java.nio.file.Path
+
+/** Provides basic metadata for a package. */
+case class PackageInfo (source :String, name :String, descrip :String)
+
+/** Provides the ability to interact with the Scaled Package Manager. */
+@Service(name="package", impl="impl.SPackageManager",
+         desc="Provides the ability to interact with the Scaled Package Manager.")
+trait PackageService {
+
+  /** Returns the names of all installed packages. */
+  def listPackages :Iterable[PackageInfo]
+
+  /** Returns the classpath for the package with source `source`.
+    * @throws NoSuchElementException if no such package exists. */
+  def classpath (source :String) :Seq[Path]
+}
