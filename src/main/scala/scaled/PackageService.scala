9f323cb03e822a9bde69863f3beeda32e3eb7c8f
Michael Bayne <mdb@samskivert.com>
2014-06-16 07:02:57 -0700

    Nix listPackages for now.

--- src/main/scala/scaled/PackageService.scala
+++ src/main/scala/scaled/PackageService.scala
@@ -6,18 +6,12 @@ package scaled
 
 import java.nio.file.Path
 
-/** Provides basic metadata for a package. */
-case class PackageInfo (source :String, name :String, descrip :String)
-
 /** Provides the ability to interact with the Scaled Package Manager. */
 @Service(name="package", impl="impl.PackageManager",
          desc="Provides the ability to interact with the Scaled Package Manager.")
 trait PackageService {
 
-  /** Returns the names of all installed packages. */
-  def listPackages :Iterable[PackageInfo]
-
-  /** Returns the classpath for the package with source `source`.
+  /** Returns the classpath for the package module with source `source`.
     * @throws NoSuchElementException if no such package exists. */
   def classpath (source :String) :Seq[Path]
 }
