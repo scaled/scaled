fb460b653d70d2764fc4b349b0be085bc2605260
Michael Bayne <mdb@samskivert.com>
2014-06-17 14:39:58 -0700

    Note to self.

--- editor/src/main/scala/scaled/impl/UndoStack.scala
+++ editor/src/main/scala/scaled/impl/UndoStack.scala
@@ -14,6 +14,7 @@ import scaled._
 class UndoStack (buffer :BufferImpl) extends Undoer {
   import UndoStack._
 
+  // TODO: dont listen to read-only buffers
   buffer.edited.onValue { edit => accum += edit }
 
   def delimitAction (point :Loc) {
