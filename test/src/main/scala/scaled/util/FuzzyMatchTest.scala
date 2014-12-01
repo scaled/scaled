7e97f24751c75b725d87d675ac1a6a8accbdccbc
Michael Bayne <mdb@samskivert.com>
2014-12-01 10:28:14 -0800

    Moved tests into test module.
    That's how we're going to roll for Scaled projects.
    
    Eventually I may convert all projects to this approach and sort things out in
    the various project implementations, but for now we'll just start with Scaled.

---
+++ test/src/main/scala/scaled/util/FuzzyMatchTest.scala
@@ -0,0 +1,23 @@
+//
+// Scaled - a scalable editor extensible via JVM languages
+// http://github.com/scaled/scaled/blob/master/LICENSE
+
+package scaled.util
+
+import org.junit.Assert._
+import org.junit._
+import scaled._
+
+class FuzzyMatchTest {
+
+  @Test def testScore () {
+    val m = FuzzyMatch("path")
+    println(m.score("ParticleCore:tripleplay.particle.ParticleShader"))
+    println(m.score("Path:playn.core"))
+    println(m.filter(Seq("ParticleCore:tripleplay.particle.ParticleShader", "Path:playn.core")))
+
+    val m2 = FuzzyMatch("path:")
+    println(m2.score("Path:pythagoras.d"))
+    println(m2.score("PathTest:pythagoras.d"))
+  }
+}
