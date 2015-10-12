33aa0f25c5c1950253046d7496fb714b8c45ef15
Michael Bayne <mdb@samskivert.com>
2015-10-11 19:58:10 -0700

    Add req with msg that throws feedback error.
    Also commented about why we can't implement get in StateV. Sigh.

--- api/src/scala/scaled/State.scala
+++ api/src/scala/scaled/State.scala
@@ -5,6 +5,7 @@
 package scaled
 
 import scala.reflect.ClassTag
+import scaled.util.Errors
 
 /** Provides a read-only view of buffer state. See [[State]]. */
 abstract class StateV {
@@ -14,10 +15,22 @@ abstract class StateV {
 
   /** A `get` variant that uses class tags to allow usage like: `get[Foo]`. */
   def get[T] (implicit tag :ClassTag[T]) :Option[T]
+  // note: we can't implement this here or Scala's overload resolution chokes... sigh
 
   /** Returns the current state value associated with `key`.
     * @throws NoSuchElementException if no value is associated with `key`. */
   def req[T] (key :Class[T]) :T
+
+  /** A `req` variant that uses class tags to allow usage like: `req[Foo]`. */
+  def req[T] (implicit tag :ClassTag[T]) :T = req(tag.runtimeClass.asInstanceOf[Class[T]])
+
+  /** Returns the current state value associated with `key`.
+    * @throws FeedbackException with `msg` no value is associated with `key`. */
+  def req[T] (key :Class[T], msg :String) :T
+
+  /** A `req` variant that uses class tags to allow usage like: `req[Foo](msg)`. */
+  def req[T] (msg :String)(implicit tag :ClassTag[T]) :T =
+    req(tag.runtimeClass.asInstanceOf[Class[T]], msg)
 
   /** Returns the keys for all currently defined state. */
   def keys :Set[Class[_]]
@@ -55,8 +68,8 @@ class RState (inits :State.Init[_]*) extends State {
   def apply[T] (key :Class[T]) :OptValue[T] = _states.get(key).asInstanceOf[OptValue[T]]
 
   /** An `apply` variant that uses class tags to allow usage like: `apply[Foo]`. */
-  def apply[T] (implicit tag :ClassTag[T]) :OptValue[T] = apply(
-    tag.runtimeClass.asInstanceOf[Class[T]])
+  def apply[T] (implicit tag :ClassTag[T]) :OptValue[T] =
+    apply(tag.runtimeClass.asInstanceOf[Class[T]])
 
   /** Returns the keys for all currently defined state. */
   def keys :Set[Class[_]] = _states.asMap.keySet.toSet
@@ -64,6 +77,11 @@ class RState (inits :State.Init[_]*) extends State {
   override def get[T] (key :Class[T]) = apply(key).getOption
   override def get[T] (implicit tag :ClassTag[T]) = apply(tag).getOption
   override def req[T] (key :Class[T]) = apply(key).get
+  override def req[T] (key :Class[T], msg :String) = {
+    val opt = apply(key)
+    if (opt.isDefined) opt.get
+    else throw Errors.feedback(msg)
+  }
   override def set[T] (key :Class[T], value :T) :Unit = apply(key).update(Some(value))
   override def clear[T] (key :Class[T]) :Unit = apply(key).update(None)
 }
